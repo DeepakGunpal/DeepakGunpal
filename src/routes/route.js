3665a0ac8cae3355aace3cd35df86296e22ef7c1
Deepak <deepakgunpal0@gmail.com>
2022-04-06 18:27:43 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,8 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    console.log('I am inside the first route')
+    res.send('Deepak gunpal first ever api!')
 });
-
+ 
+router.get('/test-me1', function (req, res) {
+    console.log('I am inside the second route')
+    res.send('Deepak gunpal second api!')
+});
+ 
+router.get('/test-me2', function (req, res) {
+    res.send('Deepak gunpal third api!')
+});
+ 
 module.exports = router;
 // adding this comment for no reason
