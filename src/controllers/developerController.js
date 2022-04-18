22181bdb92303272ad43912bd3069f10a9292a0c
Deepak <deepakgunpal0@gmail.com>
2022-04-18 18:47:35 +0530

    Finally Completed

--- src/controllers/developerController.js
+++ src/controllers/developerController.js
@@ -15,25 +15,24 @@ const scholarshipDevelopers = async function (req, res) {
 };
 
 const developers = async function (req, res) {
-  const input = req.query;
-  if(input.percentage){
-    const developer = await developerModel.find({percentage:{$gte:req.query.percentage}});
-    res.send({ data: developer });
+  const pro = await batchModel
+    .find({ program: req.query.program })
+    .select({ _id: 1 });
+  const proId = pro[0]._id;
 
-  }else{
-    const developer = await developerModel.find().populate('batch');
-    res.send({ data: developer });
-  }
- 
+  const developer = await developerModel
+    .find({
+      $and: [{ batch: proId }, { percentage: { $gte: req.query.percentage } }],
+    })
+    .populate("batch");
+  res.send({ data: developer});
 };
 
 const test = async function (req, res) {
-//   const input = req.query;
- 
-    const developer = await developerModel.find().populate('batch');
-    res.send({ data: developer });
-  
- 
+  //   const input = req.query;
+
+  const developer = await developerModel.find().populate("batch");
+  res.send({ data: developer });
 };
 
 module.exports.createDeveloper = createDeveloper;
