ea7ed5bfe824a72384f51900c06e4e46307f9b1d
Deepak <deepakgunpal0@gmail.com>
2022-08-22 23:43:09 +0530

    prototype

--- Prototype/prototype.js
+++ Prototype/prototype.js
@@ -18,7 +18,45 @@ d.__proto__ = c
 c.__proto__ = b
 b.__proto__ = a
 
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d.prop1)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d.prop1)
+
+//? functional prototype
+
+function car() {
+    this.fuel = 100;
+    this.wheels = 4;
+}
+
+car.prototype.type = "vehical"
+//? to save memory
+/**
+ function inefficientCar() {
+    this.fuel = 100;
+    this.wheels = 4;
+    this.addfuel = (amt) =>{
+        this.fuel += amt;  //* here the function is also stored in memory for every object
+    }
+}
+ */
+
+car.prototype.addfuel = function (amt) {
+    this.fuel += amt;
+}
+
+const obj = new car();
+const obj2 = new car();
+
+console.log(obj) //* car { fuel: 100, wheels: 4 }
+console.log(obj2) //* car { fuel: 100, wheels: 4 }
+console.log(obj.type) //* vehical
+
+obj.fuel = 1000
+// obj.type = "spaceship" //* will be added as new key
+
+obj.__proto__.type = "spaceship"
+
+console.log(obj.type) //* spaceship
+console.log(obj2.type) //*spaceship => this is also to spaceship
